f359f0a2475c328d252976bbb0d303e504d8c6e0
optshj <optshj@gmail.com>
2025-10-01 11:27:05 +0900

    [Release] 0.0.12

--- src/renderer/src/entities/patchNote/ui/PatchNoteModal.tsx
+++ src/renderer/src/entities/patchNote/ui/PatchNoteModal.tsx
@@ -16,6 +16,9 @@ export function PatchNoteModal() {
                 <DialogHeader>
                     <DialogTitle>새롭게 변경된 기능</DialogTitle>
                     <DialogDescription>1. 화면 조절 시 보이던 아래 여백을 없앴어요</DialogDescription>
+                    <DialogDescription>2. 일정 추가/수정 폼의 디자인이 변경되었어요</DialogDescription>
+                    <DialogDescription>3. 일정 추가/수정 시 **하루종일** 옵션을 선택할 수 있습니다 </DialogDescription>
+                    <DialogDescription>4. 기타 버그 수정 및 성능 개선되었어요</DialogDescription>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
