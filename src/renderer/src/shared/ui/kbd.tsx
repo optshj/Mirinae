de6e20d83b48491eb96b2926394d84bb7735e4fe
optshj <optshj@gmail.com>
2025-11-03 20:26:38 +0900

    [Fix] KbdGroup 시맨틱태그 수정

--- src/renderer/src/shared/ui/kbd.tsx
+++ src/renderer/src/shared/ui/kbd.tsx
@@ -16,7 +16,7 @@ function Kbd({ className, ...props }: React.ComponentProps<'kbd'>) {
 }
 
 function KbdGroup({ className, ...props }: React.ComponentProps<'div'>) {
-    return <kbd data-slot="kbd-group" className={cn('inline-flex items-center gap-1', className)} {...props} />
+    return <div data-slot="kbd-group" className={cn('inline-flex items-center gap-1', className)} {...props} />
 }
 
 export { Kbd, KbdGroup }
