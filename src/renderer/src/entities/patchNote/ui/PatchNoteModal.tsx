2443114881305b57d5c495a1d0c5541d23bd3e6e
optshj <optshj@gmail.com>
2025-11-20 11:40:50 +0900

    [Release] Release 0.1.3

--- src/renderer/src/entities/patchNote/ui/PatchNoteModal.tsx
+++ src/renderer/src/entities/patchNote/ui/PatchNoteModal.tsx
@@ -16,8 +16,8 @@ export function PatchNoteModal() {
             <DialogContent onInteractOutside={(e) => e.preventDefault()}>
                 <DialogHeader>
                     <DialogTitle>새롭게 변경된 기능</DialogTitle>
-                    <DialogDescription>1. 일부 일정의 색이 지정되지 않는 오류를 수정했습니다.</DialogDescription>
-                    <DialogDescription>2. 캘린더가 안 접히던 오류를 수정했습니다.</DialogDescription>
+                    <DialogDescription>1. 하단 일정 패널이 접히도록 개선되어 달력을 더 넓게 볼 수 있습니다.</DialogDescription>
+                    <DialogDescription>2. 일부 부자연스러운 UI 요소가 개선되었습니다.</DialogDescription>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
