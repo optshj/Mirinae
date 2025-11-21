e70e1cdb957cc4d804f1112ef534d59a976ec737
이성빈 <105402944+optshj@users.noreply.github.com>
2025-11-21 16:23:22 +0900

    [Fix] 0.1.3의 버그 수정 (#67)
    * [Fix] autoUpdate 순서 변경
    
    * [Fix] Footer가 제대로 접히게 수정, 창조절할때 transition이 적용 안되게 수정
    
    * [Fix] Footer가 제대로 접히게 수정, 창조절할때 transition이 적용 안되게 수정
    
    * [Docs] elctron-builder 최적화
    
    * [Docs] pnpm으로 교체
    
    * [Fix] 캘린더가 이상하게 올라가는 오류 수정

--- src/renderer/src/widgets/Calendar/ui/CalendarGrid.tsx
+++ src/renderer/src/widgets/Calendar/ui/CalendarGrid.tsx
@@ -37,7 +37,7 @@ export function CalendarGrid({ days, month }: Pick<DateProps, 'days' | 'month'>)
                     토
                 </div>
             </div>
-            <div className="grid h-[calc(100vh-20rem)] grid-cols-7 grid-rows-[repeat(6,1fr)] transition-all duration-300 ease-in-out [html.flip-footer_&]:h-[calc(100vh-7.5rem)]">
+            <div className="grid h-[calc(100vh-20rem)] grid-cols-7 grid-rows-[repeat(6,1fr)] transition-all duration-300 ease-in-out [html.flip-footer_&]:h-[calc(100vh-7.5rem)] [html.resizable_&]:transition-none">
                 <Dialog open={open} onOpenChange={setOpen}>
                     {days.map((date, i) => {
                         const isCurrentMonth = date.getMonth() === month;
