b0db9a1082dca7c214d33824fe01780ff70096ad
이성빈 <105402944+optshj@users.noreply.github.com>
2025-11-22 15:25:23 +0900

    [Feat] 일정을 시간순으로 자동 정렬하게 수정, 1시간 단위 새로고침 (#70)
    * [Feat] 일정을 시간순으로 자동 정렬하게 수정, 1시간 단위 새로고침
    
    * [Fix] EditEvent시 정렬이 제대로 되도록 수정
    
    * [Fix] createEventBody를 별도로 분리
    
    * [Fix] createEventBody의 타입불일치 수정

--- src/renderer/src/features/event/api/useGoogleCalendar.tsx
+++ src/renderer/src/features/event/api/useGoogleCalendar.tsx
@@ -1,56 +1,62 @@
+import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import { useShowHoliday } from '@/features/event/model/ShowHolidayContext';
-
 import { CalendarEvent, isHolidayEvent } from '@/shared/types/EventType';
 import { useLogin } from '@/shared/hooks/useLogin';
 
-/**
- * Google Calendar API를 사용하여 캘린더 이벤트를 가져오는 훅입니다.
- * 공휴일 목록과 이벤트 목록을 가져옵니다.
- * @returns {Object} - 캘린더 이벤트 목록과 공휴일 목록
- */
+const CALENDAR_API_URL = 'https://www.googleapis.com/calendar/v3/calendars';
+const REFETCH_INTERVAL = 1000 * 60 * 60; // 1 hour
+
 export function useGoogleCalendar() {
     const { tokens } = useLogin();
     const { isShow } = useShowHoliday();
-    const { data: items } = useQuery({
+
+    const { data: events } = useQuery({
         queryKey: ['googleCalendarEvents'],
         queryFn: async () => {
-            const res = await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=2500', {
+            const res = await fetch(`${CALENDAR_API_URL}/primary/events?maxResults=2500`, {
+                method: 'GET',
                 headers: { Authorization: `Bearer ${tokens.access_token}` }
             }).then((res) => res.json());
             return res;
         },
         select: (data) => {
-            return data.items.map((event: CalendarEvent) => {
-                return {
-                    ...event,
-                    colorId: event.colorId ?? '1'
-                };
-            });
+            return (data.items || []).map((event: CalendarEvent) => ({
+                ...event,
+                colorId: event.colorId ?? '1'
+            }));
         },
-        enabled: Boolean(tokens.access_token)
+        enabled: Boolean(tokens.access_token),
+        refetchInterval: REFETCH_INTERVAL
     });
-    const { data: holidayItems } = useQuery({
+
+    const { data: holidayEvents } = useQuery({
         queryKey: ['googleCalendarHolidays'],
         queryFn: async () => {
-            const res = await fetch('https://www.googleapis.com/calendar/v3/calendars/ko.south_korea%23holiday%40group.v.calendar.google.com/events?maxResults=2500', {
+            const res = await fetch(`${CALENDAR_API_URL}/ko.south_korea%23holiday%40group.v.calendar.google.com/events?maxResults=2500`, {
+                method: 'GET',
                 headers: { Authorization: `Bearer ${tokens.access_token}` }
             }).then((res) => res.json());
             return res;
         },
         select: (data) => {
-            return data.items
-                .filter((event: CalendarEvent) => isHolidayEvent(event))
-                .map((event: CalendarEvent) => {
-                    return { ...event, colorId: '10' };
-                });
+            return (data.items || []).filter((event: CalendarEvent) => isHolidayEvent(event)).map((event: CalendarEvent) => ({ ...event, colorId: '10' }));
         },
         enabled: Boolean(tokens.access_token)
     });
-    const mergedItems = isShow ? [...(holidayItems ?? []), ...(items ?? [])] : (items ?? []); // event와 holiday를 합침 (공휴일표시 여부에 따라 필터링)
+
+    const mergedEvents = isShow ? [...(holidayEvents ?? []), ...(events ?? [])] : (events ?? []);
+
+    const sortedEventsOrderByDate = useMemo(() => {
+        return [...mergedEvents].sort((a, b) => {
+            const startA = a.start.dateTime ?? a.start.date;
+            const startB = b.start.dateTime ?? b.start.date;
+            return startA.localeCompare(startB);
+        });
+    }, [mergedEvents]);
 
     return {
-        items: mergedItems
+        items: sortedEventsOrderByDate
     };
 }
