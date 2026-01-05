279c1d9f1d8fa085f3c3aa9b2e52b55d93c689d1
이성빈 <105402944+optshj@users.noreply.github.com>
2026-01-05 14:09:40 +0900

    🐛 [Fix] 일정 추가/수정 시 폼이 안 닫히는 오류 수정 (#89)

--- src/renderer/entities/patchNote/ui/PatchNoteModal.tsx
+++ src/renderer/entities/patchNote/ui/PatchNoteModal.tsx
@@ -16,11 +16,7 @@ export function PatchNoteModal() {
             <DialogContent onInteractOutside={(e) => e.preventDefault()} onEscapeKeyDown={(e) => e.preventDefault()}>
                 <DialogHeader>
                     <DialogTitle>새롭게 변경된 기능</DialogTitle>
-                    <DialogDescription>1. 일정 글자가 조금 작아졌습니다.</DialogDescription>
-                    <DialogDescription>2. 일정 추가/수정 시 간편하게 입력할 수 있도록 개선되었습니다.</DialogDescription>
-                    <DialogDescription>3. 일정을 완료 표시할 수 있도록 기능이 추가되었습니다.</DialogDescription>
-                    <DialogDescription>4. 개발자에게 문의하기 기능이 추가되었습니다. 자유롭게 문의하세요!</DialogDescription>
-                    <DialogDescription>5. 기타 여러 버그가 수정되고 성능이 향상되었습니다.</DialogDescription>
+                    <DialogDescription>1. 일정 추가/수정 시 폼이 안닫히는 오류가 수정되었습니다.</DialogDescription>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
