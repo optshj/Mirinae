dcf053e40683052b8dff414ff9c23478eef5c4ea
optshj <optshj@gmail.com>
2026-03-12 22:36:38 +0900

    🐛 [Fix] 일정 수정이 안되는 오류 수정

--- src/renderer/entities/event/api/queries.ts
+++ src/renderer/entities/event/api/queries.ts
@@ -1,6 +1,8 @@
 import { queryOptions } from '@tanstack/react-query';
 import { eventApi } from '.';
 
+const TWO_HOURS_IN_MS = 2 * 60 * 60 * 1000; // 2 hours
+
 export const eventKeys = {
   events: ['googleCalendarEvents'] as const,
   holidays: ['googleCalendarHolidays'] as const
@@ -10,11 +12,17 @@ export const eventOptions = {
   events: (accessToken: string) =>
     queryOptions({
       queryKey: eventKeys.events,
-      queryFn: () => eventApi.getEvents(accessToken)
+      queryFn: () => eventApi.getEvents(accessToken),
+      staleTime: TWO_HOURS_IN_MS,
+      refetchInterval: TWO_HOURS_IN_MS,
+      refetchIntervalInBackground: true
     }),
+
   holidays: (accessToken: string) =>
     queryOptions({
       queryKey: eventKeys.holidays,
-      queryFn: () => eventApi.getHolidays(accessToken)
+      queryFn: () => eventApi.getHolidays(accessToken),
+      staleTime: TWO_HOURS_IN_MS,
+      refetchInterval: TWO_HOURS_IN_MS
     })
 };
